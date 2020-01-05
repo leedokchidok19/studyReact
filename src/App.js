46804adabaf79b6c3b5bc1e36e25e1c538083197
leedokchidok19 <duckhyun4433@naver.com>
2020-01-05 22:49:03 +0900

    생활코딩 props, state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, {Component} from 'react';
 import Subject from "./components/Subject";
 import Toc from "./components/Toc";
+import Content from "./components/Content";
+import Alarm from "./alarm/AlarmMain";
 import './App.css';
 
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      subject:{title:"Start", sub:"신나는 리액트"},
+      mode:'welcome',
+      subject:{title:"WEB", sub:"World Wide Web!"},
+      welcome:{title:'welcome',desc:'Hello, React!!'},
       contents :[
         {id:1, title:'HTML', desc:'HTML is HyperText....'},
         {id:2, title:'CSS', desc:'CSS is for design'},
@@ -17,12 +21,19 @@ class App extends Component {
 
   }
   render(){
+    var _title, _desc = null;
+    if(this.state.mode === 'welcome'){
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    }else if (this.state.mode === 'read') {
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
     return (
       <div className="App">
           <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
-          <Subject title="react" sub="wow"></Subject>
           <Toc data={this.state.contents}></Toc>
-          <h1>hellow</h1>
+          <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
